5cda01e86f397c28030c3c958f2448f7fc0ee748
Divyanshu Bhatt <divyanshubhatt0@gmail.com>
2024-03-22 10:43:27 +0530

    add MNIST

--- main.py
+++ main.py
@@ -1,23 +1,65 @@
 import time
-from tqdm import tqdm
+import torch
+import argparse
 import numpy as np
+import torchvision
+import pandas as pd
+from tqdm import tqdm
 import matplotlib.pyplot as plt
-from compression.arithmetic import ArithmeticCoded
+import torchvision.transforms as transforms
 from compression.huffman import HuffmanCoded
+from compression.arithmetic import ArithmeticCoded
+
 from utils import sizeCalculator
 
 
+def loadMNIST(batch_size, train_bool=False, num_points=1024):
+    """
+    Load the MNIST dataset
+
+    Parameters
+    ----------
+    batch_size : int
+        The batch size
+    train_bool : bool, optional
+        If True, load the train dataset, else load the test dataset
+    num_points : int, optional
+        The number of images to load
+
+    Returns
+    -------
+    dataloader : torch.utils.data.DataLoader
+        The dataloader for the MNIST dataset
+    """
+
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.numpy()),
+            transforms.Lambda(lambda x: (x * 255).astype(np.uint8)),
+        ]
+    )
+
+    dataset = torchvision.datasets.MNIST(
+        root="./data", train=train_bool, download=True, transform=transform
+    )
+    indices = np.random.choice(len(dataset), num_points, replace=False)
+    dataset = torch.utils.data.Subset(dataset, indices)
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=True
+    )
+    return dataloader
+
+
 if __name__ == "__main__":
-    # image_names = [
-    #     "4.2.03.tiff",
-    #     "5.1.10.tiff",
-    #     "5.1.11.tiff",
-    #     "5.2.09.tiff",
-    #     "5.3.02.tiff",
-    #     "7.1.01.tiff",
-    #     "7.1.02.tiff",
-    #     "7.1.08.tiff",
-    # ]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--dataset", type=str, default="MNIST")
+    args = parser.parse_args()
+
+    batch_size = args.batch_size
+
     arithmetic_ratios = []
     huffman_ratios = []
     arithmetic_encoding_ratios = []
@@ -25,96 +67,39 @@ if __name__ == "__main__":
     arithmetic_time = []
     huffman_time = []
 
-    ##
-    import torch
-    import torchvision
-    import torchvision.transforms as transforms
+    if args.dataset == "MNIST":
+        dataloader = loadMNIST(batch_size)
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Lambda(lambda x: x.numpy())]
-    )
-
-    trainset = torchvision.datasets.MNIST(
-        root="./data", train=True, download=True, transform=transform
-    )
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=True)
-
-    num_images = 10
-
-    for i, data in enumerate(tqdm(trainloader, desc="Processing images")):
-        if i == num_images:
-            break
-        image = data[0][0]
-        image = image[0]
-        image = image.numpy()
-
-        # Convert image to 8-bit
-        image = (image * 255).astype(np.uint8)
+    for i, (batch_images, _) in enumerate(tqdm(dataloader, desc="Processing images")):
+        batch_images = batch_images.numpy()
 
         start = time.time()
-        arithmetic = ArithmeticCoded(image)
+        arithmetic = ArithmeticCoded(batch_images)
         arithmetic_time.append(time.time() - start)
 
         start = time.time()
-        huffman = HuffmanCoded(image)
+        huffman = HuffmanCoded(batch_images)
         huffman_time.append(time.time() - start)
 
         arithmetic_encoding_ratios.append(
-            sizeCalculator(arithmetic.encoding) / (np.prod(image.shape) * 8)
+            sizeCalculator(arithmetic.encoding) / (np.prod(batch_images.shape) * 8)
         )
         huffman_encoding_ratios.append(
-            sizeCalculator(huffman.encoding) / (np.prod(image.shape) * 8)
+            sizeCalculator(huffman.encoding) / (np.prod(batch_images.shape) * 8)
         )
         arithmetic_ratios.append(arithmetic.compressionRatio())
         huffman_ratios.append(huffman.compressionRatio())
 
-    plt.bar(
-        np.arange(num_images),
-        arithmetic_ratios,
-        width=0.35,
-        label="Arithmetic",
-    )
-    plt.bar(
-        np.arange(num_images) + 0.35,
-        huffman_ratios,
-        width=0.35,
-        label="Huffman",
-    )
-    plt.ylabel("Compression Ratio")
-    plt.legend()
-    plt.savefig("compression_ratios.png")
-    plt.show()
-
-    plt.bar(
-        np.arange(num_images),
-        arithmetic_encoding_ratios,
-        width=0.35,
-        label="Arithmetic",
-    )
-    plt.bar(
-        np.arange(num_images) + 0.35,
-        huffman_encoding_ratios,
-        width=0.35,
-        label="Huffman",
-    )
-    plt.ylabel("Encoding Ratio")
-    plt.legend()
-    plt.savefig("encoding_ratios.png")
-    plt.show()
-
-    plt.bar(
-        np.arange(num_images),
-        arithmetic_time,
-        width=0.35,
-        label="Arithmetic",
-    )
-    plt.bar(
-        np.arange(num_images) + 0.35,
-        huffman_time,
-        width=0.35,
-        label="Huffman",
-    )
-    plt.ylabel("Time")
-    plt.legend()
-    plt.savefig("times.png")
-    plt.show()
+        pd.DataFrame(
+            {
+                "arithmetic_including_histogram": arithmetic_ratios,
+                "huffman_including_tree": huffman_ratios,
+                "arithmetic_encoding": arithmetic_encoding_ratios,
+                "huffman_encoding": huffman_encoding_ratios,
+                "arithmetic_time": arithmetic_time,
+                "huffman_time": huffman_time,
+            }
+        ).to_csv(
+            f"./results/compression_ratios_{args.dataset}{args.batch_size}.csv",
+            index=False,
+        )

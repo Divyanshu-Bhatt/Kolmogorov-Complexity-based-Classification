dd07774b43ffae4842fc840b397215dc40a6cdb8
Divyanshu Bhatt <divyanshubhatt0@gmail.com>
2024-04-26 23:47:13 +0530

    minor fixes

--- run.sh
+++
@@ -1,3 +0,0 @@
-python main.py --batch_size=2 --dataset=MNIST --num_points=-1 --distance_metric=GZIP_NCD --k 3 5 7 9 11 13 15 17 19 21 23
-
-python description_main.py --batch_size=1 --dataset=MNIST --num_points=-1 --compression_technique=btc --block_size=8 --rank=1
